9070169b8a174e419e2d7a04fbb4fdb928079183
ryoichi7 <ryoichidoto@gmail.com>
2023-12-12 15:39:03 +0300

    finish models

--- src/main/java/ryo/market/models/Items.java
+++ src/main/java/ryo/market/models/Items.java
@@ -1,4 +1,51 @@
 package ryo.market.models;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "items")
 public class Items {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Item's name should not be empty!")
+    @Size(max = 50, message = "Item's name should not be more than 50 characters!")
+    private String name;
+
+    @Column(name = "quantity")
+    @Min(value = 0, message = "Quantity should not be less than zero!")
+    @NotNull(message = "Quantity should be a vaild number!")
+    private Integer quantity;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
--- src/main/java/ryo/market/models/Orders.java
+++ src/main/java/ryo/market/models/Orders.java
@@ -3,6 +3,8 @@ package ryo.market.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "orders")
 public class Orders {
@@ -18,5 +20,31 @@ public class Orders {
     @NotNull(message = "Order should have customer dependency!")
     private Customers customer;
 
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Customers getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customers customer) {
+        this.customer = customer;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
